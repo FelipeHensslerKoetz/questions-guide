1d42f3b4f342e898973700061b8ee4e294e357ed
Felipe Henssler Koetz <felipehenssler@gmail.com>
2021-12-24 08:45:07 -0300

    chore: render variables inside index view

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const app = express();
 
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  res.render("index");
+app.get("/:name/:language", (req, res) => {
+  const { name, language } = req.params;
+
+  res.render("index", { name, language });
 });
 
 app.listen(3000, (err) => {
