f247f4b4833ab6c81f974325a98c5a49fbe53fe3
Felipe Henssler Koetz <felipehenssler@gmail.com>
2021-12-26 15:07:09 -0300

    chore: add body-parser

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.render("index");
@@ -10,6 +13,11 @@ app.get("/", (req, res) => {
 
 app.get("/ask", (req, res) => {
   res.render("ask");
+});
+
+app.post("/question", (req, res) => {
+  console.log(req.body);
+  res.send("Request was received");
 });
 
 app.listen(3000, (err) => {
